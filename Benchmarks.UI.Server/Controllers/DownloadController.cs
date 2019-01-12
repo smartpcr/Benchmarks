a56982a387867c2716a718b131f8745ba458fc78
Sebastien Ros <sebastienros@gmail.com>
2019-01-11 18:19:00 -0800

    Adding Benchmarks.UI projects

---
+++ Benchmarks.UI.Server/Controllers/DownloadController.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Benchmarks.UI.Server.Controllers
+{
+    [Route("api/download")]
+    public class DownloadController : ControllerBase
+    {
+        static string _driverPath = @"C:\Users\sebros\Documents\Projects\benchmarks\src\BenchmarksDriver\bin\Debug\netcoreapp2.1";
+
+        [HttpGet]
+        [Route("{filename}")]
+        [RequestSizeLimit(100_000_000)]
+        public IActionResult Download(string filename)
+        {
+            return File(System.IO.File.OpenRead(Path.Combine(_driverPath, filename)), "application/object");
+        }
+    }
+}
